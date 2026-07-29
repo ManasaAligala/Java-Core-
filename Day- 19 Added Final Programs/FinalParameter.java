e794b45d88367e479124dc436b62ff9ca9ab07df
Manasa <manasaaligala502@gmail.com>
2026-07-29 23:53:54 +0530

    Updated on 29-07-2026

---
+++ Day- 19 Added Final Programs/FinalParameter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class FinalParameter {
+    int a;
+    int b;
+    final FinalParameter(int a,int b){
+        this.a = a;
+        this.b = b;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        FinalParameter p = new FinalParameter(a, b);
+
+
+    }
+    
+}
---
+++ Day- 19 Added Final Programs/ImmutableStudent.java
@@ -0,0 +1,6 @@
+public final class ImmutableStudent {
+    public static void main(String args[]){
+        final int n = 6;
+        System.out.println(n);
+    }
+}
