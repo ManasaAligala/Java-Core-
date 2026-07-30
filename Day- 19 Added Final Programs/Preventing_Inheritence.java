b07ab933add8910b6c28da607ad4d41ee6327657
Manasa <manasaaligala502@gmail.com>
2026-07-30 07:18:00 +0530

    Added day - 19 progrm

---
+++ Day- 19 Added Final Programs/Preventing_Inheritence.java
@@ -0,0 +1,13 @@
+
+public final class Preventing_Inheritence {
+    final int a = 6;
+    final String str = "manasa";
+    public static void main(String args[]){
+        Preventing_Inheritence obj = new Preventing_Inheritence();
+        System.out.println(obj.a);
+          System.out.println(obj.str);
+          System.out.println("We cannot inherit Final classes");
+
+    }
+    
+}
