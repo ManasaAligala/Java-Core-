570b1f6f26e05054f375350d59dd95fb6ad9d4c6
Manasa <manasaaligala502@gmail.com>
2026-07-30 23:17:12 +0530

    Added on 30-07-2026

--- Day- 19 Added Final Programs/FinalParameter.java
+++ Day- 19 Added Final Programs/FinalParameter.java
@@ -2,14 +2,21 @@ import java.util.Scanner;
 public class FinalParameter {
     int a;
     int b;
-    final FinalParameter(int a,int b){
+     FinalParameter(final int a,final int b){
         this.a = a;
         this.b = b;
+    }
+    void display(){
+        System.out.println(a);
+        System.out.println(b);
     }
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         FinalParameter p = new FinalParameter(a, b);
+
 
 
     }
