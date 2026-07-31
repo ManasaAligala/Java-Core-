fced2fbff0570d524251a07c651770961fb95ce7
Manasa <manasaaligala502@gmail.com>
2026-07-31 23:57:47 +0530

    added on 31-07-2026

--- Day-20 Added Interfaces/VehicleDemo.java
+++ Day-20 Added Interfaces/VehicleDemo.java
@@ -1,3 +1,31 @@
-public class VehicleDemo {
+interface Vehicle{
+   void  start();
+   void stop();
+   void fuelType();
+}
+public class VehicleDemo implements Vehicle {
+    @Override
+
+     public void start(){
+            System.out.println("Vehicle has been started");
+        }
+        @Override
+        public void stop(){
+            System.out.println("Vehicle has been stopped");
+        }
+        @Override
+        public void fuelType(){
+            System.out.println("Vehicle uses petrol");
+        }
     
+    public static void main(String[] args) {
+        VehicleDemo v = new VehicleDemo();
+        v.fuelType();
+        v.start();
+        v.stop();
+
+       
+
+    
+}
 }
