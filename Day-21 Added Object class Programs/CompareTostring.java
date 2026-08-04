370a2085d429dc14f2d130f20a36d5f6028216f8
Manasa <manasaaligala502@gmail.com>
2026-08-04 23:13:47 +0530

    added on 4-8-2026

---
+++ Day-21 Added Object class Programs/CompareTostring.java
@@ -0,0 +1,14 @@
+public class CompareTostring {
+     int id;
+    String name;
+    int marks;
+
+    public CompareTostring(int id, String name, int marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
