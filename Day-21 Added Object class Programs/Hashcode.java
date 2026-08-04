1f38360b06f1311d0f8f6e3a41d70b170b2ff00e
Manasa <manasaaligala502@gmail.com>
2026-08-04 23:14:34 +0530

    added on 4-8-2026

---
+++ Day-21 Added Object class Programs/Hashcode.java
@@ -0,0 +1,9 @@
+public class Hashcode {
+    public static void main(String args[]){
+        Hashcode h = new Hashcode();
+        String m = "manasa";
+        int hash = m.hashCode();
+
+        System.out.println(hash);
+    }
+}
